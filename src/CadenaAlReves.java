3eb0f9dabc2c3a0d4f68024eaf90ee03f0a917e0
Gustavo Starace <practicodecocina@gmail.com>
2023-01-30 18:12:30 +0100

    Initial commit

---
+++ src/CadenaAlReves.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CadenaAlReves {
+
+    public static void main(String[] args) {
+
+       
+
+        Scanner scanner = new Scanner(System.in);
+        String texto;
+        System.out.println("Introduce un texto");
+        texto = scanner.nextLine();
+
+        StringBuilder reves = new StringBuilder(texto);
+        texto = reves.reverse().toString();
+
+        System.out.println(texto);
+    }
+}
+
